3f62ca04a5d06a422bd84d00f7bb2149fa0c4d41
warrenmooretutoring <warrenmooretutoring@gmail.com>
2022-05-29 22:37:42 +0100

    Added colour picker

--- Shiny R/App_v1.r
+++ Shiny R/App_v1.r
@@ -11,6 +11,8 @@ library(shiny)
 library(tidyverse)
 library(magrittr)
 library(RColorBrewer)
+library(shinyjs)
+library(tmaptools)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -46,20 +48,19 @@ server <- function(input, output){
                             Category=c(input$IV1_level1_name, input$IV1_level2_name)) 
         } 
 
- 
     # plot in graph
     (p <- ggplot(df, aes(x=Category, y=Mean, fill = Category)) + 
         geom_bar(stat="identity") +
-        scale_fill_brewer(palette = "BuPu") +
         geom_errorbar(aes(ymin=Mean - ymincol, ymax=Mean + ymaxcol), width=.2,
                       position=position_dodge(0.05)) +
         labs(title = paste("Figure", input$fig_number),
              subtitle = input$fig_subtitle,
            #  caption = paste("italic('Note.') Error bars indicate ", input$errorBarType,". ", input$fig_caption)) +
           #  caption = paste("italic('Note.') Error bars indicate ", input$errorBarType,". ", input$fig_caption)) +
-       caption = expression(paste(italic("Note. "), "Error bars indicate ", input$errorBarType, "test text"))) +
-             xlab(input$IV_name) +
+        caption = expression(paste(italic("Note. "), "Error bars indicate ", input$errorBarType, "test text"))) +
+        xlab(input$IV_name) +
         ylab(input$DV_name) +
+        ylab(input$IV1_Level1_Color) +
         theme_classic() +
         theme(plot.title = element_text(face = "bold", size = 20),
               plot.subtitle = element_text(size = 16),
@@ -67,18 +68,25 @@ server <- function(input, output){
               axis.title.x = element_text(face = "bold", size = 14),
               axis.title.y = element_text(face = "bold", size = 14),
               axis.text = element_text(size = 12))) 
-  
+    #Toggle watermark - add watermark to plot when checkbox ticked, else load plot without
     if (input$TogglePlotWatermark == 1) {
       p + annotate("text", x = c(1, 2), y = c(60, 40), 
                    label = c("NeuroStats.co.uk") , color="blue", alpha=.2,
                    size=15 , angle=35, fontface="bold") } else {
                      p
                    }
+    #Change plot bar colours using html colour picker
+    if (input$barColor == "orange") {
+      p + scale_fill_manual(values = c("#FDDCBA", "#EC6E0F"))
+    } else {
+     p + scale_fill_brewer(palette = "BuPu") 
+    }
     
 })
-  
 }
 
+#Show colour pallets
+#tmaptools::palette_explorer()
 
 # add download button
 # output$downloadData <- downloadHandler(
